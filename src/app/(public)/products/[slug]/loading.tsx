a66fcc06fcb6ab4001f601a5ff8b4e0082046794
abdlrhmn0x7e <abdalrahman.vim@gmail.com>
2025-10-16 10:30:35 +0300

    fix(product-page): skeleton wasn't matching the page

--- src/app/(public)/products/[slug]/loading.tsx
+++ src/app/(public)/products/[slug]/loading.tsx
@@ -4,36 +4,41 @@ import { Skeleton } from "~/components/ui/skeleton";
 
 export default function ProductPageLoading() {
   return (
-    <section className="py-24 lg:py-32">
-      <MaxWidthWrapper className="relative space-y-8 lg:grid lg:grid-cols-2 lg:gap-12">
-        <div className="h-fit w-full max-w-full space-y-3 lg:sticky lg:top-32">
-          <AspectRatio ratio={16 / 12} className="w-full">
+    <section className="py-24">
+      <MaxWidthWrapper className="relative space-y-4 lg:grid lg:grid-cols-2">
+        {/* Left column: Carousel skeleton */}
+        <div className="mx-auto h-fit w-4/5 lg:sticky lg:top-24">
+          <AspectRatio ratio={1} className="w-full">
             <Skeleton className="size-full" />
           </AspectRatio>
 
-          <div className="flex gap-3">
-            <Skeleton className="size-24" />
-            <Skeleton className="size-24" />
-            <Skeleton className="size-24" />
-            <Skeleton className="size-24" />
+          <div className="mt-3 flex gap-3">
+            <Skeleton className="size-20 flex-shrink-0" />
+            <Skeleton className="size-20 flex-shrink-0" />
+            <Skeleton className="size-20 flex-shrink-0" />
+            <Skeleton className="size-20 flex-shrink-0" />
           </div>
         </div>
 
+        {/* Right column: Product details */}
         <div className="space-y-8 divide-y [&>*:not(:last-child)]:pb-8">
+          {/* First section: Product info and options */}
           <div className="space-y-8">
-            <div className="space-y-2">
-              <div className="flex flex-col justify-between gap-2 lg:flex-row">
+            {/* Title and badge */}
+            <div className="space-y-2 text-center lg:text-left">
+              <div className="flex flex-col items-center justify-between gap-2 lg:flex-row lg:items-start">
                 <Skeleton className="h-8 w-3/4" />
                 <Skeleton className="h-9 w-32" />
               </div>
 
-              <Skeleton className="h-5 w-5/6" />
-              <Skeleton className="h-5 w-2/3" />
+              <Skeleton className="mx-auto h-5 w-5/6 lg:mx-0" />
+              <Skeleton className="mx-auto h-5 w-2/3 lg:mx-0" />
             </div>
 
+            {/* Price and rating */}
             <div className="flex flex-col items-center justify-between gap-2 lg:flex-row">
               <div className="space-x-2">
-                <Skeleton className="inline-block h-8 w-40" />
+                <Skeleton className="inline-block h-10 w-40" />
                 <Skeleton className="inline-block h-6 w-24" />
               </div>
 
@@ -43,24 +48,29 @@ export default function ProductPageLoading() {
               </div>
             </div>
 
-            <div className="space-y-4">
-              <div className="flex items-center gap-2">
+            {/* Variants section */}
+            <div>
+              <div className="mb-4 flex items-center gap-2">
                 <Skeleton className="h-6 w-24" />
                 <Skeleton className="h-4 w-48" />
               </div>
 
-              <div className="space-y-4">
-                <Skeleton className="h-5 w-32" />
+              <div className="space-y-4 p-2">
+                {/* First option with thumbnails */}
+                <div className="space-y-2">
+                  <Skeleton className="h-5 w-32" />
 
-                <div className="flex flex-wrap gap-2">
-                  {[0, 1].map((i) => (
-                    <div key={i} className="flex flex-col items-center gap-2">
-                      <Skeleton className="size-18" />
-                      <Skeleton className="h-4 w-12" />
-                    </div>
-                  ))}
+                  <div className="flex flex-wrap gap-2">
+                    {[0, 1].map((i) => (
+                      <div key={i} className="flex flex-col items-center gap-2">
+                        <Skeleton className="size-18" />
+                        <Skeleton className="h-4 w-12" />
+                      </div>
+                    ))}
+                  </div>
                 </div>
 
+                {/* Other options */}
                 <div className="space-y-3">
                   {[0, 1].map((i) => (
                     <div key={i} className="space-y-2">
@@ -77,50 +87,28 @@ export default function ProductPageLoading() {
               </div>
             </div>
 
+            {/* Action buttons */}
             <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
               <Skeleton className="h-10 w-full" />
               <Skeleton className="h-10 w-full" />
             </div>
           </div>
 
+          {/* Second section: Tabs and reviews */}
           <div className="space-y-4">
+            {/* Tab triggers */}
             <div className="flex w-full gap-2">
               <Skeleton className="h-9 w-24" />
               <Skeleton className="h-9 w-24" />
               <div className="flex-1" />
             </div>
 
+            {/* Tab content - Details */}
             <div className="space-y-4">
               <Skeleton className="h-6 w-32" />
               <Skeleton className="h-4 w-full" />
               <Skeleton className="h-4 w-11/12" />
               <Skeleton className="h-4 w-10/12" />
-            </div>
-
-            <div className="space-y-3">
-              {[0, 1, 2].map((i) => (
-                <div
-                  key={i}
-                  className="flex items-start gap-3 rounded-lg border p-4"
-                >
-                  <Skeleton className="size-10 rounded-full" />
-                  <div className="w-full space-y-2">
-                    <div className="flex items-center justify-between gap-2">
-                      <div className="flex items-center gap-2">
-                        <Skeleton className="h-4 w-24" />
-                        <Skeleton className="h-5 w-28 rounded-full" />
-                      </div>
-                      <Skeleton className="h-8 w-20" />
-                    </div>
-                    <div className="flex items-center gap-2">
-                      <Skeleton className="h-5 w-24" />
-                      <Skeleton className="h-5 w-20" />
-                    </div>
-                    <Skeleton className="h-4 w-full" />
-                    <Skeleton className="h-4 w-5/6" />
-                  </div>
-                </div>
-              ))}
             </div>
           </div>
         </div>
