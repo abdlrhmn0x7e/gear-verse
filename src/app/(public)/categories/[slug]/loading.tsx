dcc95a378b58500fd25bd61cd1874bd43304b39c
abdlrhmn0x7e <abdalrahman.vim@gmail.com>
2025-11-16 12:37:09 +0200

    feat(category-products): loading state

--- src/app/(public)/categories/[slug]/loading.tsx
+++ src/app/(public)/categories/[slug]/loading.tsx
@@ -1,9 +1,15 @@
-import { Spinner } from "~/components/spinner";
+import { MaxWidthWrapper } from "~/components/max-width-wrapper";
+import { ProductsSkeleton } from "./_components/products";
+import { Skeleton } from "~/components/ui/skeleton";
 
-export default function CategoryLoading() {
+export default function LoadingCategoryProducts() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <Spinner size="page" />
-    </div>
+    <MaxWidthWrapper className="relative grid min-h-screen grid-cols-1 gap-8 py-16 pt-24 lg:grid-cols-12 lg:pt-32">
+      <aside className="sticky top-24 hidden size-full lg:col-span-4 lg:block xl:col-span-3">
+        <Skeleton className="size-full" />
+      </aside>
+
+      <ProductsSkeleton className="lg:col-span-8 xl:col-span-9" />
+    </MaxWidthWrapper>
   );
 }
